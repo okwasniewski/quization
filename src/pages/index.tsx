e5601529dd4bade0d0bd410e01913dc796b9eb88
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2021-11-24 20:19:15 +0100

    button implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Button from 'components/Button/Button';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
@@ -10,10 +9,6 @@ const Home: NextPage = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <div>
-      <Button label="Test" />
-      <Button label="Test" outline />
-      <Button label="Test" outline disabled />
-      <Button label="Test" disabled />
       <div className="alert alert-success">
         <div className="flex-1" />
       </div>
