91eaa3d83d71e83e8a9e1e4aa7df620eb064f53b
Adam Łukasiewicz <adamlukasiewicz3@gmail.com>
2021-12-08 18:57:27 +0000

    delete comments

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 
 interface InputProps {
-  type: React.HTMLInputTypeAttribute;
-  value: string | number;
-  required?: boolean;
-  placeholder?: string;
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
+    type: React.HTMLInputTypeAttribute;
+    value: string | number;
+    required?: boolean;
+    placeholder?: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 function Input({ type, value, placeholder, required, onChange }: InputProps) {
-  return (
-    <input
-      type={type}
-      value={value}
-      required={required}
-      placeholder={placeholder}
-      onChange={onChange}
-      className="font-semibold rounded-full text-gray-500 text-lg m-2 p-2 border-main border-2 bg-white px-5 py-1"
-    />
-  );
+    return (
+        <input
+            type={type}
+            value={value}
+            required={required}
+            placeholder={placeholder}
+            onChange={onChange}
+            className="p-2 px-5 py-1 m-2 text-lg font-semibold text-gray-500 bg-white border-2 rounded-full border-main"
+        />
+    );
 }
 
 export default Input;
-
-//   className={`px-10 m-2 py-1 rounded-full text-lg  font-semibold transition-all transform hover:scale-110 disabled:opacity-50 disabled:scale-100 disabled:cursor-not-allowed ${
