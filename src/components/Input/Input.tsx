6988411371d64964ac9b132b3ba818cf2c5fd6a7
Adam Łukasiewicz <adamlukasiewicz3@gmail.com>
2021-12-08 18:42:34 +0000

    18 create input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface InputProps {
+    type: React.HTMLInputTypeAttribute,
+    value: string | number;
+    required?: boolean;
+    placeholder?: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+function Input({type,value,placeholder,required,onChange}: InputProps) {
+    return <input 
+        type={type}
+        value={value}
+        required={required}
+        placeholder={placeholder} 
+        onChange={onChange} 
+        className={`font-semibold rounded-full text-gray-500 text-lg m-2 p-2 border-main border-2 bg-white px-5 py-1`}    
+    />
+}
+
+export default Input;
+
+    //   className={`px-10 m-2 py-1 rounded-full text-lg  font-semibold transition-all transform hover:scale-110 disabled:opacity-50 disabled:scale-100 disabled:cursor-not-allowed ${
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Input from 'components/Input/Input';
 
 const Home: NextPage = () => (
   <>
