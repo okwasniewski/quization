3da006a655a345c9da47e386955ec81f896d1565
Oskar Kwasniewski <okwasniewski@macbook-pro-oskar.home>
2021-12-15 19:13:21 +0100

    Add header buttons & links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
 import Button from 'components/Button/Button';
 import React from 'react';
-import Quization from '../../../public/assets/Quization.svg';
+import Link from 'next/link';
+import Quization from 'assets/Quization.svg';
+import { useRouter } from 'next/router';
 
-const Header = () => (
-  <div className="navbar mb-2 shadow-lg text-neutral-content bg-white p-2">
-    <div className="flex-1 hidden px-2 mx-2 lg:flex">
-      <Quization />
+const Header = () => {
+  const router = useRouter();
+  return (
+    <div className="navbar mb-4 shadow-md text-neutral-content bg-white p-3">
+      <div className="container mx-auto">
+        <div className="flex-1 hidden px-2 mx-2 lg:flex">
+          <Link href="/" passHref>
+            <a>
+              <Quization />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Button
+            label="Zaloguj się"
+            outline
+            onClick={() => router.push('/login')}
+          />
+          <Button
+            label="Rejestracja"
+            onClick={() => router.push('/register')}
+          />
+        </div>
+      </div>
     </div>
-    <div>
-      <Button label="Login" onClick={() => {}} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
