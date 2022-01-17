6f234ffa13de184866c64b8aed6e7bff022d67fa
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-01-17 08:29:40 +0100

    add: responsive hero component, not working background

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Hero = () => (
+  <div
+    className="rounded-2xl h-auto w-auto"
+    style={{ backgroundImage: "url('../../assets/Hero.png')" }}
+  >
+    <div className="px-5 pb-12 text-center lg:pl-12 pt-36 lg:text-left">
+      <h1 className="text-xl font-bold text-white lg:text-3xl">
+        Europejskie ramy kompetencji cyfrowych
+      </h1>
+      <p className="pt-2 text-base text-white lg:text-xl">
+        Kompetencje cyfrowe, obok czytania, pisania, umiejętności matematycznych
+        i językowych, <br /> stanowią zespół fundamentalnych umiejętności
+        współczesnego człowieka.
+      </p>
+    </div>
+  </div>
+);
+
+export default Hero;
