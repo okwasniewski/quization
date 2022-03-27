f7560efa3e3868ba251c6f1d54ce5eec294ef37d
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2022-03-27 19:59:06 +0200

    Guard routes for unauthorized access

--- src/templates/AuthorizedTemplate.tsx
+++ src/templates/AuthorizedTemplate.tsx
@@ -3,6 +3,10 @@ import Footer from 'components/Footer/Footer';
 import SidebarPanel from 'components/SidebarPanel/SidebarPanel';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useCallback, useEffect } from 'react';
+import { useAuth } from './AuthorizedContext';
+import { auth } from '../firebase';
 
 interface AuthorizedTemplateProps {
   title: string;
@@ -14,19 +18,44 @@ const AuthorizedTemplate: NextPage<AuthorizedTemplateProps> = ({
   title,
   description,
   children,
-}: AuthorizedTemplateProps) => (
-  <>
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <SidebarPanel userName="Jan Kowalski">
-      <Header authorized />
-      <div className="container px-5 py-3 mx-auto min-h-screen">{children}</div>
-      <Footer authorized />
-    </SidebarPanel>
-  </>
-);
+}: AuthorizedTemplateProps) => {
+  const router = useRouter();
+  const { setUser, user } = useAuth();
+
+  useEffect(() =>
+    auth.onAuthStateChanged((userObj) => {
+      if (userObj) {
+        setUser(userObj);
+      } else {
+        setUser(null);
+        router.push('/login');
+      }
+    })
+  );
+  const handleLogout = useCallback(async () => {
+    await auth.signOut();
+    setUser(null);
+    router.push('/login');
+  }, [setUser, router]);
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SidebarPanel
+        userName={user?.displayName || ''}
+        avatarImage={user?.photoURL || ''}
+      >
+        <Header authorized handleLogout={handleLogout} />
+        <div className="container px-5 py-3 mx-auto min-h-screen">
+          {children}
+        </div>
+        <Footer authorized />
+      </SidebarPanel>
+    </>
+  );
+};
 
 export default AuthorizedTemplate;
