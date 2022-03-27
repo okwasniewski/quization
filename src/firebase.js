fad04adf1f5d53f8bfad0e45999c3139e35eadcf
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2022-03-27 18:34:42 +0200

    Remove firebase config variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,22 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import {
   GoogleAuthProvider,
-  GithubAuthProvider,
   getAuth,
   signInWithPopup,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
   signOut,
-  FacebookAuthProvider
-} from 'firebase/auth';
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from 'firebase/firestore';
+  FacebookAuthProvider,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGl75B0O_F4RFlFKvUAxCQyIe68PdBmU8",
-  authDomain: "quization-9a464.firebaseapp.com",
-  projectId: "quization-9a464",
-  storageBucket: "quization-9a464.appspot.com",
-  messagingSenderId: "203891306053",
-  appId: "1:203891306053:web:0377b6f5242f759853fd2c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -35,7 +26,6 @@ const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 const fbProvider = new FacebookAuthProvider();
 
-
 const GoogleLogin = (cb) => {
   signInWithPopup(auth, googleProvider)
     .then((result) => {
@@ -44,7 +34,7 @@ const GoogleLogin = (cb) => {
       const token = credential?.accessToken;
       // The signed-in user info.
       const user = result.user;
-      cb()
+      cb();
       // ...
     })
     .catch((error) => {
@@ -58,7 +48,7 @@ const GoogleLogin = (cb) => {
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
-}
+};
 
 const FacebookLogin = (cb) => {
   signInWithPopup(auth, fbProvider)
@@ -68,7 +58,7 @@ const FacebookLogin = (cb) => {
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       const credential = FacebookAuthProvider.credentialFromResult(result);
       const accessToken = credential?.accessToken;
-      cb()
+      cb();
       // ...
     })
     .catch((error) => {
@@ -82,12 +72,12 @@ const FacebookLogin = (cb) => {
       // const credential = FacebookAuthProvider.credentialFromError(error);
       // ...
     });
-}
+};
 
 const emailLogin = (cb) => {
   signInWithEmailAndPassword(auth, email.value, password.value)
     .then(() => {
-      cb()
+      cb();
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -100,7 +90,7 @@ const emailLogin = (cb) => {
 const emailRegister = (cb) => {
   createUserWithEmailAndPassword(auth, email.value, password.value)
     .then(() => {
-      cb()
+      cb();
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -108,7 +98,6 @@ const emailRegister = (cb) => {
       alert(errorMessage);
     });
 };
-
 
 const logout = () => {
   signOut(auth);
