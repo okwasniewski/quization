787c134231f36dad66147341c5423a80992f6227
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2022-03-27 19:58:28 +0200

    Add auth context for storing User data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
 import 'styles/style.css';
+import { AuthContextProvider } from 'templates/AuthorizedContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 
 export default MyApp;
