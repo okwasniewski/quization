4746cff41f170706ca73c08a69d002c501ef5afd
Ryczko <konradryczko1@gmail.com>
2022-04-03 12:15:49 +0200

    Improve summary views

--- src/pages/quiz/[quizId].tsx
+++ src/pages/quiz/[quizId].tsx
@@ -1,10 +1,13 @@
 import Hero from 'components/Hero/Hero';
+import Image from 'next/image';
 import Question from 'components/Question/Question';
 import { addDoc, collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import AuthorizedTemplate from 'templates/AuthorizedTemplate';
 import { Attempt, Question as QuestionTypeArr, Quiz } from 'types/quiz';
+import Button from 'components/Button/Button';
+import { Router, useRouter } from 'next/router';
 import { db, auth } from '../../firebase';
 
 interface SingleQuizProps {
@@ -17,6 +20,8 @@ function SingleQuiz({ questions, quiz }: SingleQuizProps) {
   const elapsedTime = useRef(new Date());
   const [activeQuestion, setActiveQuestion] = useState<number>(0);
   const [currentAnswer, setCurrentAnswer] = useState<string>('');
+  const router = useRouter();
+
   const [answers, setAnswers] = useState<
     { answer: string; correct: boolean }[]
   >([]);
@@ -56,12 +61,14 @@ function SingleQuiz({ questions, quiz }: SingleQuizProps) {
       alert('Podejście zapisane');
     }
   };
+
   useEffect(() => {
     if (activeQuestion === questions.length) {
       handleSaveAttempt();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeQuestion]);
+
   return (
     <AuthorizedTemplate title="Quiz" description="Strona quizu">
       <Hero
@@ -94,15 +101,29 @@ function SingleQuiz({ questions, quiz }: SingleQuizProps) {
         {activeQuestion === questions?.length && (
           <div className="max-w-sm overflow-hidden shadow-lg py-8 px-4 bg-white rounded-xl min-w-full flex flex-col text-center items-center justify-center my-4">
             {correctAnswers / answers?.length < 0.5 ? (
-              <h2 className="text-2xl font-bold">
-                Niestety, nie udalo ci się :(
-              </h2>
+              <>
+                <h2 className="text-2xl font-bold mb-6">
+                  Niestety, nie udalo ci się :(
+                </h2>
+                <Image src="/failed.svg" width={200} height={200} />
+              </>
             ) : (
-              <h2 className="text-2xl font-bold">Gratulacje, udało ci się!</h2>
+              <>
+                <h2 className="text-2xl font-bold mb-6">
+                  Gratulacje, udało ci się!
+                </h2>
+                <Image src="/done.svg" width={200} height={200} />
+              </>
             )}
             <h3 className="text-xl font-bold mt-4">
               Liczba poprawnych odpowiedzi: {correctAnswers}/{answers.length}
             </h3>
+            <div className="mt-4 flex flex-wrap">
+              <Button onClick={() => router.push('/panel')} outline>
+                Rozwiąż inny quiz
+              </Button>
+              <Button onClick={() => router.reload()}>Rozwiąż ponownie</Button>
+            </div>
           </div>
         )}
       </div>
